3362ccd50504b2736dc1f691fd6722808dd5d3bb
Juliodvp29 <juliodevep29@gmail.com>
2022-11-21 14:44:57 -0500

    services

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/interfaces/user.interface';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  public users: User[] = [];
+  public search: string = '';
+
+  constructor(
+    private userService: UserService
+  ) { }
 
   ngOnInit(): void {
+   this.userService.getAllUsers().subscribe((resp: any) => {
+     this.users = resp
+   })
+  }
+
+  onSearchUser(search: string){
+    this.search = search;
   }
 
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { FetchAllUser, User } from '../interfaces/user.interface';
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  private url: string = "https://jsonplaceholder.typicode.com/users";
+
+  constructor(private http: HttpClient) { }
+
+  getAllUsers(): Observable<User[]>{
+    return this.http.get<User[]>(this.url)
+    .pipe(
+      map(((result) => {
+        const data = result.map((u) => {
+          return{
+            id: u.id,
+            name: u.name,
+            username: u.username,
+            email: u.email
+          }
+        })
+        return data;
+      }))
+    )
+  }
+
 }
